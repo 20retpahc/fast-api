da6fe9865c13536b4295a44c1395a9231f789caf
20retpahc <20retpahc@gmail.com>
2025-10-01 16:23:19 +0900

    check

--- app/features/admin/country/infrastructure/model/country_model.py
+++ app/features/admin/country/infrastructure/model/country_model.py
@@ -12,4 +12,4 @@ class CountryModel(Base):
     country_code = Column(String, nullable=False)
 
     created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), nullable=True , onupdate=func.now())
+    updated_at = Column(DateTime(timezone=True), nullable=True, onupdate=func.now())
